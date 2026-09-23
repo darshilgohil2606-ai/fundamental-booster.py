bbbe4c7be60c850e1f362630026d238f6d4c3ceb
darshilgohil2606-ai <darshilgohil2606@gmail.com>
2026-09-23 16:12:04 +0530

    Add files via upload

---
+++ fundamental_booster.py
@@ -0,0 +1,21 @@
+print("Welcome to the Interactive Data Collector!\n")
+print()
+
+a=input("Please Enter your name: ")
+b=int(input("Please Enter your age: "))
+c=float(input("Please enter your height in meters: "))
+e=int(input("Please enter your favorite number: "))
+print()
+
+
+print("Thank you! Here is the information we collected:\n")
+print()
+
+
+print("Name:", a ,"(Type is:",type(a),"Memory Address:",id(a),")")
+print("Age:", b ,"(Type is:",type(b),"Memory Address:",id(b),")")
+print("Height:", c ,"(Type is:",type(c),"Memory Address:",id(c),")")
+print("Favorite Number:", e ,"(Type is:",type(e),"Memory Address:",id(e),")")
+print()
+
+print("thank you for using the personal data collector.good bye!")
